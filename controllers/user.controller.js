dd02d4ed5aecc7405d7bb1385e6033a0da23e56d
relifevn <thinhhuynh3108@gmail.com>
2019-12-18 01:16:36 +0900

    API test: user login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,13 @@
 const db = require('../db');
 
 const userLogin = async (req, res, next) => {
+
+  const { username, password } = req.body
+
+  console.log(username, password)
+
   try {
-    
+    res.json({ success: true })
   } catch (err) {
     res.status(500).json({ error: { message: 'Something is wrong!' } })
   }
